e1b809330fb43c552cd9fef6f5848ba19c6d6d3a
Tom Bloor <tom.bloor@googlemail.com>
2016-09-24 16:25:31 +0100

    Modify main to have menu bar less window

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const electron = require('electron')
-const {app, BrowserWindow} = electron
+const {app, BrowserWindow} = require('electron')
 
 app.on('ready', () => {
-  let win = new BrowserWindow({width:800, height: 600})
-  win.loadURL(`file://${__dirname}/index.html`)
-  win.webContents.openDevTools()
+  let win = new BrowserWindow({
+    width:800,
+    height: 600,
+    autoHideMenuBar: true
+  })
+  win.loadURL(`file://${__dirname}/src/index.html`)
+  //win.webContents.openDevTools()
 })
